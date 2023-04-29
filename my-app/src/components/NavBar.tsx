f381c49eceaded473d14b04fa95de98a1e10073d
Danoonka <bogdanaostrovska986@gmail.com>
2023-04-29 18:32:34 +0300

    Add key attribute to mp generated components rewrite condition rendering

--- my-app/src/components/NavBar.tsx
+++ my-app/src/components/NavBar.tsx
@@ -4,7 +4,16 @@ import Button from "../utils/Button";
 import {Link} from "react-router-dom";
 import aboutPageCompanyImg from "../assets/about-page-company-img.png";
 
-let user = {id: 4,  "user_email": "string", "user_firstname": "string", "user_lastname": "string", "user_avatar": aboutPageCompanyImg, "user_status": "string", "user_city": "string", "user_phone": "string"}
+let user = {
+    id: 4,
+    "user_email": "string",
+    "user_firstname": "string",
+    "user_lastname": "string",
+    "user_avatar": aboutPageCompanyImg,
+    "user_status": "string",
+    "user_city": "string",
+    "user_phone": "string"
+}
 
 const NavBar = () => {
     const [isAuth, setIsAuth] = useState(false)
@@ -12,24 +21,23 @@ const NavBar = () => {
         <nav className="nav">
             <a href="/meduzzen-demo" className="nav-title">INK</a>
             <ul>
-                <li><Link  to="/about"> About</Link></li>
+                <li><Link to="/about"> About</Link></li>
                 <li><Link to="/userList">User List</Link></li>
                 <li><Link to="/companyList">Company List</Link></li>
             </ul>
-            {!isAuth &&
-            <div>
-                <Link to='/userRegistration'>
-                    <Button>Sign up</Button>
-                </Link>
+            {isAuth
+                ?   <Link to='/userProfile' state={{data: user}}>
+                        <Button>User Name</Button>
+                    </Link>
+                :   <div>
+                        <Link to='/userRegistration'>
+                            <Button>Sign up</Button>
+                        </Link>
 
-                <Link to='/userAuthorization'>
-                    <Button>Login in </Button>
-                </Link>
-            </div> }
-            {isAuth &&
-            <Link to='/userProfile' state={{data: user}}>
-                <Button>User Name</Button>
-            </Link>
+                        <Link to='/userAuthorization'>
+                            <Button>Login in </Button>
+                        </Link>
+                    </div>
             }
         </nav>
     );
